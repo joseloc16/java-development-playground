52e100e2673178ac6fd0e22fd1406f7fd94e2785
jnunez <darksoul.js17@gmail.com>
2024-04-19 18:22:22 -0500

    estructura inicial

---
+++ src/main/java/org/joseloc/javadevelopmentplayground/JavaDevelopmentPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package org.joseloc.javadevelopmentplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevelopmentPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevelopmentPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/joseloc/javadevelopmentplayground/JavaDevelopmentPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joseloc.javadevelopmentplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevelopmentPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
